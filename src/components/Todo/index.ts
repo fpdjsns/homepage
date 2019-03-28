8f950173a8b58d4279e4a4aff57a69dcec482b4d
fpdjsns <withham1@naver.com>
2019-03-28 21:22:32 +0900

    [TODO] use table, style object

--- src/components/Todo/index.ts
+++ src/components/Todo/index.ts
@@ -1,34 +1,25 @@
 import { Vue, Component, Prop } from "vue-property-decorator";
 import { mapState, mapActions } from "../../../node_modules/vuex";
 
-// class TodoItem {
-//     name: string = '';
-//     todo: string = '';
-
-//     constructor(name: string, todo: string) {
-//         this.name = name;
-//         this.todo = todo;
-//     }
-// }
-
 @Component({
     template: require('./Todo.html'),
     computed: {
         ...mapState('todo', ['todos'])
     },
-    // methods: {
-    //     ...mapActions('todo', ['getTodos'])
-    // }
 })
 export default class Todo extends Vue {
-    // todos: TodoItem[] = [
-    //     new TodoItem("아침","늦잠자라"),
-    //     new TodoItem("점심","낮잠자라"),
-    //     new TodoItem("아침","이제자라")
-    // ];
     created() {
         console.log(process.env.NOW);
         this.$store.dispatch('todo/getTodos');
+    }
+
+    // Style
+    styleThead: object = {
+        color: 'red'
+    }
+
+    styleTbody: object = {
+        color: 'blue'
     }
 
 }
